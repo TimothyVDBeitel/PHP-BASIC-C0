95b9a0db974e4be1c22c5c4c2a80f0ecaea2dbd1
Timothy van den Beitel <2157002@talnet.nl>
2021-12-22 12:18:18 +0100

    2.6

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,16 @@
+<?php
+$naam = "Timothy";
+$komma = 3.99;
+$yesorno = true;
+$getal = 3;
+$x = null;
+
+echo gettype ($naam);
+echo "</br>";
+echo gettype ($komma);
+echo "</br>";
+echo gettype ($yesorno);
+echo "</br>";
+echo gettype ($getal);
+echo "</br>";
+echo gettype ($x);
