98b0b1d1b0d658f9e11d87d373b64c1adf30b177
Timothy van den Beitel <2157002@talnet.nl>
2021-12-23 12:04:35 +0100

    3.2

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,19 @@
+<?php
+
+$myDogNames= ["Bulldog","Malteser","King Charles"];
+
+echo $myDogNames[2];
+echo "</br>";
+echo $myDogNames[1];
+echo "</br>";
+echo $myDogNames[0];
+
+echo "</br>";
+echo "</br>";
+
+echo $myDogNames[2]= "Chihuahua";
+echo "</br>";
+echo $myDogNames[1];
+echo "</br>";
+echo $myDogNames[0];
+
