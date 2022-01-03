809aff7b16dba42bee1980d0c0ffef14c9afc0ac
Timothy van den Beitel <2157002@talnet.nl>
2022-01-03 15:53:16 +0100

    5.1

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,18 @@
+<?php
+$test1= 12;
+$test2= "gewoon tekst";
+echo var_dump($test2 == $test1);
+echo "</br>";
+echo var_dump($test2 === $test1);
+echo "</br>";
+echo var_dump($test2 != $test1);
+echo "</br>";
+echo var_dump($test2 > $test1);
+echo "</br>";
+echo var_dump($test2 < $test1);
+echo "</br>";
+echo var_dump($test2 <> $test1);
+echo "</br>";
+echo var_dump($test2 >= $test1);
+echo "</br>";
+echo var_dump($test2 <= $test1);
