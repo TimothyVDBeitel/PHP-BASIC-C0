5bf2bffe0d99d045cf32d4a831c14ec878fb6014
Timothy van den Beitel <2157002@talnet.nl>
2022-01-04 11:56:17 +0100

    5.2

---
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -0,0 +1,13 @@
+<?php
+$test1 = 10;
+$test2 = 6;
+
+if ($test1 == $test2) echo "gelijk";
+if ($test1 != $test2) echo "ongelijk";
+if ($test1 === $test2) echo "identiek";
+echo "</br>";
+if ($test1 > $test2) echo "test1 is groter dan test2";
+if ($test1 < $test2) echo "test1 is kleiner dan test2";
+echo "</br>";
+if ($test1 >= $test2) echo "test1 is groter of gelijk aan test2";
+if ($test1 <= $test2) echo "test1 is kleiner of gelijk aan test2";
